02d4ea7196ace3b653475dd5510787f1b725b1b9
Muhanad Atef <muhanad.atef23@gmail.com>
2019-03-21 17:57:41 +0200

    open community

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { CommunityComponent } from './community/community.component';
 
 const routes: Routes = [
   { path: 'profile/:username', component: ProfileComponent },
+  { path: 'community/:CommunityName', component: CommunityComponent }
 ];
 
 @NgModule({
--- src/app/classes/user-public-info.ts
+++ src/app/classes/user-public-info.ts
@@ -3,12 +3,14 @@ export class UserPublicInfo {
     public karma: number;
     public cake_day: string;
     public about: string;
-    public profile_pic: string;
-    constructor(username: string, karma: number,cake_day: string,about: string,profile_pic: string) {
+    public photo_path: string;
+    public cover_path: string;
+    constructor(username: string, karma: number,cake_day: string,about: string,photo_path: string,cover_path: string) {
         this.username = username;
         this.karma = karma;
-        this.cake_day=cake_day;
-        this.about=about;
-        this.profile_pic=profile_pic;
+        this.cake_day = cake_day;
+        this.about = about;
+        this.photo_path = photo_path;
+        this.cover_path = cover_path;
     }
 }
