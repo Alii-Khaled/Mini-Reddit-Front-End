1f2bc78260821354f4773c553f56ecff01521c77
mmmacmp <mmmacmp@gmail.com>
2019-03-22 17:26:51 +0200

    fixing git repo

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,13 +1,21 @@
 import { Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, from} from 'rxjs';
-import { catchError, tap} from 'rxjs/operators';
+import { Observable} from 'rxjs';
+import { catchError, tap} from 'rxjs/operators'; 
+
+import { UserCommunities } from './classes/user-communities';
+import { UserPublicInfo } from './classes/user-public-info';
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class HttpService {
     constructor(private http: HttpClient) {}
+    GetCommNameAndLogo() :Observable<any>
+    {
+        return this.http.get("http://localhost:3000/communities");
+    }
 
     GetMyCommunities(): Observable<UserCommunities[]> {
         return this.http.get<UserCommunities[]>('http://localhost:3000/communities');
@@ -15,4 +23,5 @@ export class HttpService {
     GetUserPublicInfo(id: number): Observable<UserPublicInfo> {
         return this.http.get<UserPublicInfo>('http://localhost:3000/user_public_info/' + id);
     }
-}
+    }
+
