7c6b9e23a3f38da74ff8ec05cba7eda689c4df98
Muhanad Atef <muhanad.atef23@gmail.com>
2019-03-23 13:22:52 +0200

    Muhanad : Documentation

--- src/app/classes/user-public-info.ts
+++ src/app/classes/user-public-info.ts
@@ -1,11 +1,34 @@
+/**
+ * this class to store user public info
+ */
 export class UserPublicInfo {
+    /**
+     * username of this user
+     */
     public username: string;
+    /**
+     * karma of this user
+     */
     public karma: number;
-// tslint:disable-next-line: variable-name
+    /**
+     * birth day of this user
+     */
     public cake_day: string;
+    /**
+     * about for this user
+     */
     public about: string;
+    /**
+     * profile pic URL of this user
+     */
     public photo_path: string;
+    /**
+     * banner pic URL of this user
+     */
     public cover_path: string;
+    /**
+     * constructor takes every value of the class and assign it to this user
+     */
     constructor(username: string, karma: number,cake_day: string,about: string,photo_path: string,cover_path: string) {
         this.username = username;
         this.karma = karma;
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -15,10 +15,16 @@ export class HttpService {
     GetCommNameAndLogo(): Observable<any> {
         return this.http.get('http://localhost:3000/communities');
     }
-
+    /**
+     * to get all communities subscribed by this user
+     */
     GetMyCommunities(): Observable<UserCommunities[]> {
         return this.http.get<UserCommunities[]>('http://localhost:3000/communities');
     }
+    /**
+     * get user public info like (karma,name,username,...)
+     * @param id now we use id to get specific user but when connect to back-end we will use username
+     */
     GetUserPublicInfo(id: number): Observable<UserPublicInfo> {
         return this.http.get<UserPublicInfo>('http://localhost:3000/user_public_info/' + id);
     }
