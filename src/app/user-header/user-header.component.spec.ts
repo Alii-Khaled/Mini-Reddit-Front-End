70dfdb56a7c2bdd0359d80dcd0b36d91b2b9b720
Muhanad Atef <muhanad.atef23@gmail.com>
2019-03-24 06:23:10 +0200

    imports for unit testing

--- src/app/user-header/user-header.component.spec.ts
+++ src/app/user-header/user-header.component.spec.ts
@@ -2,6 +2,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UserHeaderComponent } from './user-header.component';
 import { HttpClientModule} from '@angular/common/http';
+import { By, BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamicTesting, BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { ProfileComponent } from '../profile/profile.component';
+import { CommunityComponent } from '../community/community.component';
+import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
+import { PostLayoutComponent } from '../post-layout/post-layout.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatIconModule, MatCardModule, MatToolbarModule,
+   MatSidenavModule, MatListModule, MatStepperModule, MatInputModule } from '@angular/material';
+
+
+
 
 describe('UserHeaderComponent', () => {
   let component: UserHeaderComponent;
@@ -9,13 +22,37 @@ describe('UserHeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientModule],
-      declarations: [ UserHeaderComponent ]
+      imports: [RouterTestingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule ,
+        MatStepperModule,
+        MatInputModule],
+
+      declarations: [
+          UserHeaderComponent,
+          ProfileComponent,
+          CommunityComponent,
+          LoginComponent,
+          SignupComponent,
+          PostLayoutComponent],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting());
     fixture = TestBed.createComponent(UserHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -27,7 +64,7 @@ describe('UserHeaderComponent', () => {
 
   it('should create', () => {
     component.PublicInfo = {
-      "username": "7moda",
+      'username': "7moda",
       "name": "7moda basta",
       "karma": 500,
       "cake_day": "March 8, 2019",
@@ -35,5 +72,10 @@ describe('UserHeaderComponent', () => {
       "photo_path": "https://avatars1.githubusercontent.com/u/47671499?s=400&v=4",
       "cover_path": "https://avatars1.githubusercontent.com/u/47671499?s=400&v=4"
     };
+    fixture.detectChanges();
+    const titleElement: HTMLElement = fixture.debugElement.query( By.css('#logo')).nativeElement;
+
+
+    expect(titleElement.innerText).toContain('');
 });
 });
