33f47f526e8b1e169e2b97730c006358a3720621
Muhanad Atef <muhanad.atef23@gmail.com>
2019-03-24 07:48:33 +0200

    Muhanad: unit testing

--- src/app/user-header/user-header.component.spec.ts
+++ src/app/user-header/user-header.component.spec.ts
@@ -4,15 +4,20 @@ import { UserHeaderComponent } from './user-header.component';
 import { HttpClientModule} from '@angular/common/http';
 import { By, BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamicTesting, BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { ProfileComponent } from '../profile/profile.component';
-import { CommunityComponent } from '../community/community.component';
 import { LoginComponent } from '../login/login.component';
 import { SignupComponent } from '../signup/signup.component';
-import { PostLayoutComponent } from '../post-layout/post-layout.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule, MatIconModule, MatCardModule, MatToolbarModule,
    MatSidenavModule, MatListModule, MatStepperModule, MatInputModule } from '@angular/material';
-
+import { CUSTOM_ELEMENTS_SCHEMA , DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '../app-routing.module';
+import { ProfileComponent } from '../profile/profile.component';
+import { CommunityComponent } from '../community/community.component';
+import { PostLayoutComponent } from '../post-layout/post-layout.component';
 
 
 
@@ -31,20 +36,33 @@ describe('UserHeaderComponent', () => {
         MatIconModule,
         MatCardModule,
         MatToolbarModule,
+        RouterModule,
         MatButtonModule,
         MatSidenavModule,
         MatIconModule,
         MatListModule ,
         MatStepperModule,
-        MatInputModule],
+        MatInputModule,
+        BrowserModule,
+        AppRoutingModule,
+        AngularFontAwesomeModule,
+        HttpClientModule,
+        AngularFontAwesomeModule,
+        ModalModule.forRoot(),
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        FormsModule,
+    ],
 
       declarations: [
           UserHeaderComponent,
-          ProfileComponent,
-          CommunityComponent,
           LoginComponent,
           SignupComponent,
+          ProfileComponent,
+          CommunityComponent,
           PostLayoutComponent],
+
+      schemas: [NO_ERRORS_SCHEMA , CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -64,18 +82,16 @@ describe('UserHeaderComponent', () => {
 
   it('should create', () => {
     component.PublicInfo = {
-      'username': "7moda",
-      "name": "7moda basta",
-      "karma": 500,
-      "cake_day": "March 8, 2019",
-      "about": "be or not to be",
-      "photo_path": "https://avatars1.githubusercontent.com/u/47671499?s=400&v=4",
-      "cover_path": "https://avatars1.githubusercontent.com/u/47671499?s=400&v=4"
+      username: '7moda',
+      'name': '7moda basta',
+      'karma': 500,
+      'cake_day': 'March 8, 2019',
+      'about': 'be or not to be',
+      'photo_path': 'https://avatars1.githubusercontent.com/u/47671499?s=400&v=4',
+      'cover_path': 'https://avatars1.githubusercontent.com/u/47671499?s=400&v=4'
     };
     fixture.detectChanges();
-    const titleElement: HTMLElement = fixture.debugElement.query( By.css('#logo')).nativeElement;
-
-
-    expect(titleElement.innerText).toContain('');
+    const titleElement: HTMLElement = fixture.debugElement.query( By.css('#UserName')).nativeElement;
+    expect(titleElement.innerText).toContain('7moda');
 });
 });
