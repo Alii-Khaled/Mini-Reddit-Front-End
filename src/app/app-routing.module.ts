1c7fa22ce5392ce87cbcc062b670eca6e0a77a10
Muhanad Atef <muhanad.atef23@gmail.com>
2019-04-08 04:25:26 +0200

    merge conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { CommunityComponent } from './community/community.component';
-<<<<<<< HEAD
 import { AccountSettingComponent } from './account-setting/account-setting.component';
 import { ProfileSettingComponent } from './profile-setting/profile-setting.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import {ForgotUsernameComponent } from './forgot-username/forgot-username.component'
+import {ForgotUsernameComponent } from './forgot-username/forgot-username.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 const routes: Routes = [
 
@@ -17,18 +17,12 @@ const routes: Routes = [
   { path: './profile-setting', component: ProfileSettingComponent },
   { path: './login', component: LoginComponent },
   { path: './signup', component: SignupComponent},
-  { path: './forgot-username', component: ForgotUsernameComponent}
-
-
-=======
-import { UserSettingsComponent } from './user-settings/user-settings.component';
-
-const routes: Routes = [
+  { path: './forgot-username', component: ForgotUsernameComponent},
   { path: 'user/:username', component: ProfileComponent },
   { path: 'community/:CommunityName', component: CommunityComponent },
-  { path: 'settings', component: UserSettingsComponent }
->>>>>>> profile
+  { path: 'settings', component: UserSettingsComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
