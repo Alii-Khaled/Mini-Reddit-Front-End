a80f9c3fd4d7ddeabe5f31c212f9733526599fce
Amira <mehriz.amira@gmail.com>
2019-04-13 18:34:23 +0200

    fix somethings
    removed roterlinks in login & signup components (no need)
    tryed to fix the side photo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { ProfileComponent } from './Profile_Components/profile/profile.component
 import { CommunityComponent } from './community/community.component';
 import { AccountSettingComponent } from './account-setting/account-setting.component';
 import { ProfileSettingComponent } from './profile-setting/profile-setting.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import {ForgotUsernameComponent } from './forgot-username/forgot-username.component';
 import { UserSettingsComponent } from './user-settings/user-settings.component';
 import { OverviewComponent } from './Profile_Components/overview/overview.component';
@@ -22,8 +20,6 @@ const routes: Routes = [
 
   { path: 'account-setting', component: AccountSettingComponent },
   { path: 'profile-setting', component: ProfileSettingComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent},
   { path: 'forgot-username', component: ForgotUsernameComponent},
   { path: 'user/:username', component: ProfileComponent ,
     children: [
