d054d5b49ad446f175a25da8951ec5caf4d94e8e
Amira <mehriz.amira@gmail.com>
2019-04-14 23:34:37 +0200

    fixed small errs in (forgot-username)&&(next-page)

--- src/app/forgot-username/forgot-username.component.ts
+++ src/app/forgot-username/forgot-username.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
+import {HttpService} from '../http.service';
+import {FormBuilder, FormControlName , FormGroup , Validator, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-username',
