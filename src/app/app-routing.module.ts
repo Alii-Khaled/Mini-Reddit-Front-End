df605956c6171f2ce5c1e6d7064b57e89c228a19
mmmacmp <mmmacmp@gmail.com>
2019-04-20 21:28:00 +0200

    working on moderators page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './Profile_Components/profile/profile.component';
 import { CommunityComponent } from './community/community.component';
 import { AccountSettingComponent } from './account-setting/account-setting.component';
 import { ProfileSettingComponent } from './profile-setting/profile-setting.component';
-import {ForgotUsernameComponent } from './forgot-username/forgot-username.component';
+import { ForgotUsernameComponent } from './forgot-username/forgot-username.component';
 import { UserSettingsComponent } from './user-settings/user-settings.component';
 import { OverviewComponent } from './Profile_Components/overview/overview.component';
 import { PostsComponent } from './Profile_Components/posts/posts.component';
@@ -14,14 +14,16 @@ import { HiddenComponent } from './Profile_Components/hidden/hidden.component';
 import { UpvotedComponent } from './Profile_Components/upvoted/upvoted.component';
 import { DownvotedComponent } from './Profile_Components/downvoted/downvoted.component';
 import { EditCommunityComponent } from './edit-community/edit-community.component';
+import { CommunityModeratorsComponent } from './community-moderators/community-moderators.component';
 
 const routes: Routes = [
 
 
   { path: 'account-setting', component: AccountSettingComponent },
   { path: 'profile-setting', component: ProfileSettingComponent },
-  { path: 'forgot-username', component: ForgotUsernameComponent},
-  { path: 'user/:username', component: ProfileComponent ,
+  { path: 'forgot-username', component: ForgotUsernameComponent },
+  {
+    path: 'user/:username', component: ProfileComponent,
     children: [
       { path: '', component: OverviewComponent },
       { path: 'posts', component: PostsComponent },
@@ -34,9 +36,9 @@ const routes: Routes = [
   },
   { path: 'community/:CommunityName', component: CommunityComponent },
   { path: 'community/:CommunityName/Edit_community', component: EditCommunityComponent },
+  { path: 'community/:CommunityName/Moderators', component: CommunityModeratorsComponent },
 
-
-  { path: 'settings', component: UserSettingsComponent },
+{ path: 'settings', component: UserSettingsComponent },
 ];
 
 
