70c5e26b77d7089a42e7ef084ea1e235c7a43198
mmmacmp <mmmacmp@gmail.com>
2019-05-02 02:27:10 +0200

    get comm posts request and view it

--- src/app/community/community.http.service.ts
+++ src/app/community/community.http.service.ts
@@ -63,7 +63,7 @@ export class communityHttpService {
              * From the mock server if "IsApi" is false
              * And from Api if it is true
              */
-          
+
             return this.http.post<communityModerators[]>('http://localhost:3000/get_my_moderators/', body);
         }
         else {
@@ -246,5 +246,36 @@ export class communityHttpService {
     }
 
 
+    /**
+     *get Community posts 
+     *@param community_id now we use id to edit to Specific Community
+  
+     */
+    getCommunityPosts(community_id: number) {
+        var token = localStorage.getItem('token');
+        let headers = {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json",
+        }
+
+        let body = {
+            "community_id": community_id,
+
+        }
+        /**
+         * Choose from where i'll get my data
+         */
+        if (this.IsApi === false) {
+            return this.http.get<any>("http://localhost:3000/posts")
+        }
+        else {
+          //  return this.http.get<any>("http://35.204.169.121/api/unauth/ViewPosts", body, { headers })
+        }
+    }
+
 }
+
+
+
 
