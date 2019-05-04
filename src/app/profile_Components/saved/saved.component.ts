9f1af1e75a5c90587037df97e13c6f36f2567f73
Muhanad Atef <muhanad.atef23@gmail.com>
2019-05-04 20:27:04 +0200

    Saved links

--- src/app/profile_Components/saved/saved.component.ts
+++ src/app/profile_Components/saved/saved.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileHttpService } from '../profile.http.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-saved',
@@ -6,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saved.component.css']
 })
 export class SavedComponent implements OnInit {
-
-  constructor() { }
+  /**
+   * To hlod the url
+   */
+  a: string[];
+  /**
+   * To overview
+   */
+  saved: any[];
+  /**
+   * User name for the profile owner
+   */
+  username: string;
+  /**
+   * @param http For requests
+   * @param router For navigation
+   */
+  constructor(private http: ProfileHttpService, private router: Router , private route: ActivatedRoute) {
+    route.params.subscribe(val => {
+      /**
+       * Getting profile owner username
+       */
+       this.a = this.router.url.split('/');
+       this.username = this.a[this.a.length - 1];
+       this.saved = [];
+      /**
+       * Request for saved links
+       */
+       this.http.getSaved(this.username).subscribe((data: any[]) => {
+       this.saved = data ;
+    });
+    });
+  }
 
   ngOnInit() {
   }
