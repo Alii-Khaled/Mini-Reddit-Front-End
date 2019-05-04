55d566e2f52c3dd0e751715785cd37d0d415cb92
Muhanad Atef <muhanad.atef23@gmail.com>
2019-05-04 20:09:42 +0200

    routing

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DropdownService } from '../dropdown.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-settings',
@@ -8,7 +9,11 @@ import { DropdownService } from '../dropdown.service';
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor(private dropdown: DropdownService) { 
+  constructor(private dropdown: DropdownService , private router: Router) {
+    if (localStorage.length === 0) {
+      this.router.navigateByUrl('error');
+      return;
+    }
     this.dropdown.changeData('User Settings', 'https://i.redd.it/x0iiv7jthx901.png');
   }
 
